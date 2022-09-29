5b2f1e016782bfee578be2315dc9c892e84b963d
Vaishali Jeenwal <jeenwal.v@northeastern.edu>
2022-09-28 17:08:32 -0700

    This commit includes creation of web application as per assignment first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get("/healthz", (req, res) =>{
+    res.status(200).send("Healthy!")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
