b553eb0db56f8a71ff748c121bae998569230ff9
Vaishali Jeenwal <jeenwal.v@northeastern.edu>
2022-10-21 01:37:27 -0700

    Adding systemd config

--- app-packer.sh
+++ app-packer.sh
@@ -23,12 +23,24 @@ unzip webapp.zip
 echo "NPM Install"
 sudo npm install
 
+echo "Copying index.js to Systemd"
+sudo cp /home/ubuntu/index.service /etc/systemd/system/index.service
+
 #Configure Database
-# echo "Configuring Mysql"
-# sudo mysql -u root
-# flush privileges;
+echo "Configuring Mysql"
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
 # ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 # exit
+
+echo "Reload Demon"
+sudo systemctl daemon-reload
+
+echo "Start Systemd"
+sudo systemctl enable index.service
+
+sudo systemctl restart index.service
+
+sudo systemctl start index.service
 
 # #npm run
 # sudo npm start
